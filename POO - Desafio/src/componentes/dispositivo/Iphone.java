2db91fcc0babb916b5865c9de0a5ed4f28da39a3
Petrik <wallacepetrik@gmail.com>
2024-07-04 21:43:00 -0300

    Interface
    Add classes, interface e seus métodos

---
+++ POO - Desafio/src/componentes/dispositivo/Iphone.java
@@ -0,0 +1,53 @@
+package componentes.dispositivo;
+
+import componentes.aparelho.telefoneco.AparelhoTelefonico;
+import componentes.navegador.internet.NavegadorInternet;
+import componentes.reprodutor.musical.ReprodutorMusical;
+
+
+public class Iphone implements ReprodutorMusical,AparelhoTelefonico,NavegadorInternet {
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVOz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
---
+++ POO - Desafio/src/componentes/reprodutor/musical/ReprodutorMusical.java
@@ -0,0 +1,7 @@
+package componentes.reprodutor.musical;
+
+public interface ReprodutorMusical {
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica(String musica);
+}
